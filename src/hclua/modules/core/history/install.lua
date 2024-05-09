cb5c994ee1ca37cbedf8bc378c167e00490c34bd
Jarlyyn <jarlyyn@qq.com>
2024-05-09 23:21:31 +0800

    update

--- src/hclua/modules/core/history/install.lua
+++ src/hclua/modules/core/history/install.lua
@@ -1,8 +1,11 @@
 return function(runtime)
     local history=runtime:requireModule('compontent/history/history.lua')
+    local lineutils=runtime:requireModule('compontent/lineutils/lineutils.lua')
+
     runtime.HC.history=history.new(200)
     runtime.HC.eventBus:bindEvent('world.lineInit',function (line)
         runtime.HC.history:onLine(line)
     end)
     runtime.HC.recorder=runtime.HC.history:createRecorder()
+    runtime.HC.lineutils=lineutils
 end
